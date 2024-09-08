76f99480a7ce6845c556a8618e0852e87d2c6ace
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-08 17:22:32 +0800

    Subject: Body

--- src/components/NavBars/Navbar1.tsx
+++ src/components/NavBars/Navbar1.tsx
@@ -6,7 +6,13 @@ export const Navbar1 = () => {
     return (
 
         <>
-        
+            <nav className="navbar bg-body-tertiary">
+                <div className="container">
+                    <a className="navbar-brand" href="#">
+                        <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24"/>
+                    </a>
+                </div>
+            </nav>
         </>
 
     ) ;
