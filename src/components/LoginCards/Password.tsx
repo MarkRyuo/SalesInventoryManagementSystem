86d24de8dd87171140d81b89d0e140fab374b2ab
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-08 16:43:38 +0800

    Subject: Body

--- src/components/LoginCards/Password.tsx
+++ src/components/LoginCards/Password.tsx
@@ -5,7 +5,11 @@ export const Password = () => {
 
     return (
         <>
-
+            <div className="mb-3">
+                    <label htmlFor="username" className="form-label">Username: </label>
+                    <input type="text" className="username form-control"/>
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            </div>
         </>
     ) ;
 
