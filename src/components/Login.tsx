c1cf3fcc568b3857a2ac0d652057da22b493fb72
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-08 16:03:50 +0800

    Subject: Body

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -9,7 +9,10 @@ export const Login = () => {
 
             <form action="" className="form-login">
 
-                
+                <div>
+                    <label htmlFor="username">Username: </label>
+                    <input type="text" className="username"/>
+                </div>
 
                 <label htmlFor="password">Password: </label>
                 <input type="text" className="password"/>
