0377483c00d5ec3fd0ea86b5001a8d20620b4e60
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-08 17:39:17 +0800

    Subject: Body

--- src/components/NavBars/Navbar1.tsx
+++ src/components/NavBars/Navbar1.tsx
@@ -6,19 +6,19 @@ export const Navbar1 = () => {
     return (
 
         <>
-            <nav className="navbar mb-5">
-                <div className="row">
-                    <div className="col">
-                        <a className="navbar-brand" href="#">
-                            <img src="/public/icons8-boxLogo.svg" alt="Bootstrap" width="70" height="70"/>
-                        </a>
-                    </div>
+            <div className="row">
+                <nav className="navbar mb-5">
+                        <div className="col-4">
+                            <a className="navbar-brand" href="#">
+                                <img src="/public/icons8-boxLogo.svg" alt="Bootstrap" width="70" height="70"/>
+                            </a>
+                        </div>
 
-                    <div className="col">
-                        <p className="lead">Sales Inventory</p>
-                    </div>
-                </div>
-            </nav>
+                </nav>
+                        <div className="col-6">
+                            <p className="lead">Sales Inventory</p>
+                        </div>
+            </div>
         </>
 
     ) ;
