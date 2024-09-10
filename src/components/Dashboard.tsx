fac1410af1309eb2c1c5b5544d9e17abb0716f57
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-10 22:19:58 +0800

    Subject: Body

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -4,10 +4,16 @@
 export const Dashboard = () => {
 
     // * Main Component of dashboard
+
+    // * aside, navbar, main
     return (
       <>
         <div className="container-lg">
-  
+            <div className="row">
+                <div className="col"></div>
+                <div className="col"></div>
+                <div className="col"></div>
+            </div>
         </div>
       </>
     );
