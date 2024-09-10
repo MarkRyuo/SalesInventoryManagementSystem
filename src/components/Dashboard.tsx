47bf8ff83d01a7680c1489c6ec5efa7c1c424724
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-10 21:21:40 +0800

    Subject: Body

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -9,16 +9,12 @@ export const Dashboard = () => {
     // * Main Component of dashboard
     return (
       <>
-        <div className="container-lg">
-            <div className="row">
-                <div className="col-4">
-                    <Navbar2/>
-                </div>
-
-                <div className="col-4">
-                    <SideBar/>
-                </div>
-            </div>
+        <div className="container text-center">
+          <div className="row">
+            <div className="col">Column</div>
+            <div className="col">Column</div>
+            <div className="col">Column</div>
+          </div>
         </div>
       </>
     );
