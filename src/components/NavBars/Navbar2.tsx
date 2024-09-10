a4edb336890e650e507acc62813eaf191155396a
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-10 21:10:31 +0800

    Subject: Body

--- src/components/NavBars/Navbar2.tsx
+++ src/components/NavBars/Navbar2.tsx
@@ -8,7 +8,7 @@ export const Navbar2 = () => {
     return (
 
         <>
-            <div className="container-lg">
+            <div>
                 <div style={{border: "1px solid", width: "75vw", height: "10vh"}}>
                     Navbar
                 </div>
