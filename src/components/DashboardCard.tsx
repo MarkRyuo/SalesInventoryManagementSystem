426268416c4b776cd569c250550e3ad3d225e803
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-10 22:14:04 +0800

    Subject: Body

--- src/components/DashboardCard.tsx
+++ src/components/DashboardCard.tsx
@@ -1,5 +1,4 @@
-import { SideBar } from "./DashboardCards/Sidebar";
-import { Navbar2 } from "./NavBars/Navbar2";
+
 
 
 export const DashboardCard = () => {
@@ -8,10 +7,7 @@ export const DashboardCard = () => {
 
         <>
             <div className="container-lg">
-                <div className="row">
-                    <div className="col-12"><Navbar2/></div>
-                    <div className="col-12"><SideBar/></div>
-                </div>
+            
             </div>
         </>
     ) ;
