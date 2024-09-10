7492c41a26cd8edd2bf442010109fe0750b2f9ce
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-10 22:13:46 +0800

    Subject: Body

--- src/components/DashboardCards/MainDash.tsx
+++ src/components/DashboardCards/MainDash.tsx
@@ -1,3 +1,5 @@
+import { Navbar2 } from "../NavBars/Navbar2";
+import { SideBar } from "./Sidebar";
 
 
 
@@ -6,6 +8,13 @@ export const MainDash = () => {
     return (
 
         <>
+
+        <div className="container-lg">
+                <div className="row">
+                    <div className="col-12"><Navbar2/></div>
+                    <div className="col-12"><SideBar/></div>
+                </div>
+            </div>
 
         </>
     ) ;
