66ee63c9081d32c63e5022f1238985f1f68c6062
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-10 21:22:10 +0800

    Subject: Body

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -11,7 +11,7 @@ export const Dashboard = () => {
       <>
         <div className="container text-center">
           <div className="row">
-            <div className="col">Column</div>
+            <div className="col"><SideBar/></div>
             <div className="col">Column</div>
             <div className="col">Column</div>
           </div>
