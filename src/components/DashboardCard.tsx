d70ca43196f4445f1310d197644ee706e0df918a
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-10 21:55:59 +0800

    Subject: Body

--- src/components/DashboardCard.tsx
+++ src/components/DashboardCard.tsx
@@ -5,7 +5,12 @@ export const DashboardCard = () => {
     return (
 
         <>
-        
+            <div className="container-lg">
+                <div className="row">
+                    <div className="col-12"><Navbar2/></div>
+                    <div className="col-12"><SideBar/></div>
+                </div>
+            </div>
         </>
     ) ;
 }
