72d0005d3b2434113dc209dfca459c232ed5f32a
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-10 14:56:50 +0800

    Subject: Body

--- src/components/DashboardCards/Sidebar.tsx
+++ src/components/DashboardCards/Sidebar.tsx
@@ -7,7 +7,7 @@ export const SideBar = () => {
 
         <aside>
 
-            <div className="container-lg">
+            <div className="container-lg mb-2">
                 <div style={{border: "1px solid", width: "20vw", height: "80vh"}}>
                     Side Bar
                 </div>
