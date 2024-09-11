4940c9516416290932fddb8e1ade15bd6772f819
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-11 20:34:27 +0800

    Subject: Body

--- src/components/DashboardCards/MainDash.tsx
+++ src/components/DashboardCards/MainDash.tsx
@@ -21,18 +21,6 @@ export const MainDash = () => {
                             col-2
                         </div>
                     </div>
-
-                    <div className="col-12 col-lg-3 g-1">
-                        <div style={{height: "150px", width:"250px", border: "1px solid"}}>
-                            col-3
-                        </div>
-                    </div>
-
-                    <div className="col-12 col-lg-3 g-1">
-                        <div style={{height: "150px", width:"250px", border: "1px solid"}}>
-                            col-4
-                        </div>
-                    </div>
                     
                 </div>
             </div>
