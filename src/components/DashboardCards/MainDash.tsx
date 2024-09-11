655e4457ff9b199a9f952b330c6d26b945f3f8fd
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-11 13:36:18 +0800

    Subject: Body

--- src/components/DashboardCards/MainDash.tsx
+++ src/components/DashboardCards/MainDash.tsx
@@ -8,12 +8,12 @@ export const MainDash = () => {
         <>
 
         <div>
-            <div className="ms-2 mt-2" style={{width: "1069px",  height: "80vh", border: "solid 1px"}}>
+            <div className="m-2" style={{width: "1069px",  height: "80vh", border: "solid 1px"}}>
                 <div className="row text-center">
-                    <div className="col-3" style={{height: "150px", width:"150px", border: "1px solid"}}>col-1</div>
-                    <div className="col-3" style={{height: "150px", width:"150px", border: "1px solid"}}>col-2</div>
-                    <div className="col-3" style={{height: "150px", width:"150px", border: "1px solid"}}>col-3</div>
-                    <div className="col-3" style={{height: "150px", width:"150px", border: "1px solid"}}>col-4</div>
+                    <div className="col-3 m-2" style={{height: "150px", width:"250px", border: "1px solid"}}>col-1</div>
+                    <div className="col-3 m-2" style={{height: "150px", width:"250px", border: "1px solid"}}>col-2</div>
+                    <div className="col-3 m-2" style={{height: "150px", width:"250px", border: "1px solid"}}>col-3</div>
+                    <div className="col-3 m-2" style={{height: "150px", width:"250px", border: "1px solid"}}>col-4</div>
                 </div>
             </div>
         </div>
