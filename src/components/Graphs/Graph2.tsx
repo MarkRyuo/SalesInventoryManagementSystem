9aff5f5c797e5176399b31176cd45f9843d0d85d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-11 20:53:43 +0800

    Subject: Body

--- src/components/Graphs/Graph2.tsx
+++ src/components/Graphs/Graph2.tsx
@@ -11,7 +11,7 @@ export const Graph2 = () => {
         <>
             <div className="row text-center">
                     <div className="col-12 col-lg-3 g-1">
-                        <div style={{height: "150px", width:"250px", border: "1px solid"}}>
+                        <div className="" style={{height: "150px", width:"250px", border: "1px solid"}}>
                             col-3
                         </div>
                     </div>
