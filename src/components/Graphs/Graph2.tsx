df2dda733a9e99041cc3ea19e058eefff2411e8b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-11 20:41:51 +0800

    Subject: Body

---
+++ src/components/Graphs/Graph2.tsx
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+export const Graph2 = () => {
+
+    return (
+
+        <>
+            <div className="row text-center">
+                    <div className="col-12 col-lg-3 g-1">
+                        <div style={{height: "150px", width:"250px", border: "1px solid"}}>
+                            col-3
+                        </div>
+                    </div>
+
+                    <div className="col-12 col-lg-3 g-1">
+                        <div style={{height: "150px", width:"250px", border: "1px solid"}}>
+                            col-4
+                        </div>
+                    </div>
+                </div>
+        </>
+    ) ;
+}
