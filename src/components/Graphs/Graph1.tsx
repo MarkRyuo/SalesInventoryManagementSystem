e994ef1a3efd0fc3d4bae90cb436ee7e5f36bfa4
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-11 20:58:46 +0800

    Subject: Body

--- src/components/Graphs/Graph1.tsx
+++ src/components/Graphs/Graph1.tsx
@@ -8,13 +8,13 @@ export const Graph1 = () => {
 
         <>
             <div className="row text-center">
-                    <div className="col-6 col-lg-3 g-1">
+                    <div className="col-2 col-lg-3 g-1">
                         <div style={{height: "150px", width:"100%", border: "1px solid"}}>
                             col-1
                         </div>
                     </div>
 
-                    <div className="col-6 col-lg-3 g-1">
+                    <div className="col-2 col-lg-3 g-1">
                         <div style={{height: "150px", width:"100%", border: "1px solid"}}>
                             col-2
                         </div>
