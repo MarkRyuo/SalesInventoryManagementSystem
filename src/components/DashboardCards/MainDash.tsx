c0001b14c8de59c806927c7c67be3e43f41557d1
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-11 20:42:22 +0800

    Subject: Body

--- src/components/DashboardCards/MainDash.tsx
+++ src/components/DashboardCards/MainDash.tsx
@@ -8,7 +8,7 @@ export const MainDash = () => {
         <>
 
         <div>
-            <div className="m-2 container-lg" style={{width: "1069px",  height: "80vh",}}>
+            <div className="container-lg m-2" style={{width: "1069px",  height: "80vh",}}>
                 <div className="row text-center">
                     <div className="col-12 col-lg-3 g-1">
                         <div style={{height: "150px", width:"250px", border: "1px solid"}}>
