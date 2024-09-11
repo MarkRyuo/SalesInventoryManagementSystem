6b2fd079eed0cf2ff628dffa9b264cde776a1122
MarkRyuo <malupajhonmark3035@gmail.com>
2024-09-11 20:39:44 +0800

    Subject: Body

--- src/components/Graphs/Graph1.tsx
+++ src/components/Graphs/Graph1.tsx
@@ -7,7 +7,17 @@ export const Graph1 = () => {
     return (
 
         <>
-        
+            <div className="container">
+                        <div style={{height: "150px", width:"250px", border: "1px solid"}}>
+                            col-1
+                        </div>
+                    </div>
+
+                    <div className="col-12 col-lg-3 g-1">
+                        <div style={{height: "150px", width:"250px", border: "1px solid"}}>
+                            col-2
+                        </div>
+                    </div>
         </>
     ) ;
 }
